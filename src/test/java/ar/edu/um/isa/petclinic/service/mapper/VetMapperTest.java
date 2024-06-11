c008b7bb882fe33e577c307ede0b58c75c1c0994
Cristian Chiappone <cristian.chiappone@mercadolibre.com>
2024-06-11 18:25:35 -0300

    add test

--- src/test/java/ar/edu/um/isa/petclinic/service/mapper/VetMapperTest.java
+++ src/test/java/ar/edu/um/isa/petclinic/service/mapper/VetMapperTest.java
@@ -1,7 +1,9 @@
 package ar.edu.um.isa.petclinic.service.mapper;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
+import ar.edu.um.isa.petclinic.domain.Vet;
+import ar.edu.um.isa.petclinic.service.dto.VetDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +14,38 @@ class VetMapperTest {
     @BeforeEach
     public void setUp() {
         vetMapper = new VetMapperImpl();
+    }
+
+    @Test
+    public void testNullVetToEntity() {
+        Vet vet = vetMapper.toEntity((VetDTO) null);
+
+        assertNull(vet);
+    }
+
+    @Test
+    public void testVetToEntity() {
+        VetDTO dto = new VetDTO();
+        dto.setFirstname("set first_name");
+        dto.setLastname("set last_name");
+
+        Vet vet = vetMapper.toEntity(dto);
+
+        assertNotNull(vet);
+        assertEquals("set first_name", vet.getFirstname());
+        assertEquals("set last_name", vet.getLastname());
+    }
+    
+
+    @Test
+    public void testPartialUpdate() {
+        Vet vet = new Vet();
+        VetDTO dto = new VetDTO();
+        dto.setFirstname("updated first name");
+        dto.setLastname("updated last name");
+        vetMapper.partialUpdate(vet, dto);
+
+        assertEquals("updated first name", vet.getFirstname());
+        assertEquals("updated last name", vet.getLastname());
     }
 }
